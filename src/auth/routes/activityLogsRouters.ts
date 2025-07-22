139facc8308f7eb1aa03136ad859ff59bb5854fe
Haha <haidanglinnk0@gmail.com>
2025-07-22 21:51:32 +0700

    update

--- src/auth/routes/activityLogsRouters.ts
+++ src/auth/routes/activityLogsRouters.ts
@@ -4,10 +4,10 @@ import { authorization, checkPermission} from '../middleware';
 import {insertActivityLogs, getActivityLogs,getActivityLogsByUsername,deleteActivityLogs} from '../controllers/activityLogControllers';
 // Định tuyến cho trang activity logs
 router.get('/:username', getActivityLogsByUsername);
-
+//DeleteActivityLogs
 //chú ý route cha phải được đặt sau route con
 router.get('/',authorization,checkPermission({rightCodes: ["GetActivityLogs"], isAllowChildZone: true}), getActivityLogs);
-router.post('/',authorization,checkPermission({rightCodes: ["PostActivityLogs"], isAllowChildZone: true}), insertActivityLogs);
-router.delete('/',authorization,checkPermission({rightCodes: ["DeleteActivityLogs"], isAllowChildZone: true}), deleteActivityLogs);
+router.post('/',authorization,checkPermission({rightCodes: ["PostActivityLogs"], isAllowChildZone: false}), insertActivityLogs);
+router.delete('/',authorization,checkPermission({rightCodes: ["DeleteActivityLogs"], isAllowChildZone: false}), deleteActivityLogs);
 
 export default router;
