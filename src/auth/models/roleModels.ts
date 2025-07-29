f460399974539d9794e4f051a4f8aea05ac1b2b2
Haha <haidanglinnk0@gmail.com>
2025-07-29 14:44:33 +0700

    sua lai validate role

--- src/auth/models/roleModels.ts
+++ src/auth/models/roleModels.ts
@@ -11,6 +11,73 @@ export type role = {
     description?: string;
 };
 
+const roleInsertRule: RuleSchema = {
+    id: {
+        type: 'string',
+        required: false,
+        format: 'uuid'
+    },
+    name: {
+        type: 'string',
+        required: true,
+        max: 50,
+        noCheckXSS: false
+    },
+    code: {
+        type: 'string',
+        required: true,
+        max: 50
+    },
+    description: {
+        type: 'string',
+        required: false,
+        noCheckXSS: false
+    },
+    createdAt: {
+        type: 'string',
+        required: false,
+        format: 'datetime'
+    },
+    createdBy: {
+        type: 'string',
+        required: false,
+        max: 100
+    }
+}
+
+const roleUpdateAndDeleteRule: RuleSchema = {
+    id: {
+        type: 'string',
+        required: true,
+        format: 'uuid'
+    },
+    name: {
+        type: 'string',
+        required: false,
+        max: 50,
+        noCheckXSS: false
+    },
+    code: {
+        type: 'string',
+        required: false,
+        max: 50
+    },
+    description: {
+        type: 'string',
+        required: false,
+    },
+    createdAt: {
+        type: 'string',
+        required: false,
+        format: 'date'
+    },
+    createdBy: {
+        type: 'string',
+        required: false,
+        max: 100
+    }
+}
+
 
 export async function getRole(code: string) {
     const sqlQuery = "SELECT * FROM roles WHERE code = ?";
@@ -25,41 +92,7 @@ export async function getRoles() {
 
 
 export async function insertRole(role: role): Promise<{ data: Object | null, status: boolean, errorCode: string | Object }> {
-    const roleRule: RuleSchema = {
-        id: {
-            type: 'string',
-            required: false,
-            format: 'uuid'
-        },
-        name: {
-            type: 'string',
-            required: true,
-            max: 50,
-            noCheckXSS: false
-        },
-        code: {
-            type: 'string',
-            required: true,
-            max: 50
-        },
-        description: {
-            type: 'string',
-            required: false,
-            noCheckXSS: false
-        },
-        createdAt: {
-            type: 'string',
-            required: false,
-            format: 'datetime'
-        },
-        createdBy: {
-            type: 'string',
-            required: false,
-            max: 100
-        }
-    }
-
-    const { status, results } = validateDataArray([role], roleRule, messagesEn);
+    const { status, results } = validateDataArray([role], roleInsertRule, messagesEn);
     if (status) {
         return await insertObject("roles", role);
     }
@@ -68,40 +101,7 @@ export async function insertRole(role: role): Promise<{ data: Object | null, sta
 
 
 export async function updateRole(rolesId: string, role: role): Promise<{ data: Object | null, status: boolean, errorCode: string | Object }> {
-    const roleRule: RuleSchema = {
-        id: {
-            type: 'string',
-            required: true,
-            format: 'uuid'
-        },
-        name: {
-            type: 'string',
-            required: false,
-            max: 50,
-            noCheckXSS: false
-        },
-        code: {
-            type: 'string',
-            required: false,
-            max: 50
-        },
-        description: {
-            type: 'string',
-            required: false,
-        },
-        createdAt: {
-            type: 'string',
-            required: false,
-            format: 'date'
-        },
-        createdBy: {
-            type: 'string',
-            required: false,
-            max: 100
-        }
-    }
-
-    const { status, results } = validateDataArray([role], roleRule, messagesEn);
+    const { status, results } = validateDataArray([role], roleUpdateAndDeleteRule, messagesEn);
     if (status) {
         const columKey = { id: rolesId }; // Use userId as the columKey
         return await updateObject("roles", role, columKey);
@@ -111,40 +111,8 @@ export async function updateRole(rolesId: string, role: role): Promise<{ data: O
 
 
 export async function deleteRole(rolesId: string | number): Promise<{ data: Object | null, status: boolean, errorCode: string | Object }> {
-    const roleRule: RuleSchema = {
-        id: {
-            type: 'string',
-            required: true,
-            format: 'uuid'
-        },
-        name: {
-            type: 'string',
-            required: false,
-            max: 50,
-            noCheckXSS: false
-        },
-        code: {
-            type: 'string',
-            required: false,
-            max: 50
-        },
-        description: {
-            type: 'string',
-            required: false,
-        },
-        createdAt: {
-            type: 'string',
-            required: false,
-            format: 'date'
-        },
-        createdBy: {
-            type: 'string',
-            required: false,
-            max: 100
-        }
-    }
     const columKey = { id: rolesId }; // Use userId as the columKey
-    const { status, results } = validateDataArray([columKey], roleRule, messagesEn);
+    const { status, results } = validateDataArray([columKey], roleUpdateAndDeleteRule, messagesEn);
     if (status) {
 
         return await deleteObject("roles", columKey);
@@ -154,42 +122,7 @@ export async function deleteRole(rolesId: string | number): Promise<{ data: Obje
 
 
 export async function insertRoles(roles: Array<role>): Promise<{ data: Object | null, status: boolean, errorCode: string | Object }> {
-
-    const roleRule: RuleSchema = {
-        id: {
-            type: 'string',
-            required: false,
-            format: 'uuid'
-        },
-        name: {
-            type: 'string',
-            required: true,
-            max: 50,
-            noCheckXSS: false
-        },
-        code: {
-            type: 'string',
-            required: true,
-            max: 50
-        },
-        description: {
-            type: 'string',
-            required: false,
-            noCheckXSS: false
-        },
-        createdAt: {
-            type: 'string',
-            required: false,
-            format: 'datetime'
-        },
-        createdBy: {
-            type: 'string',
-            required: false,
-            max: 100
-        }
-    }
-
-    const { status, results } = validateDataArray(roles, roleRule, messagesEn);
+    const { status, results } = validateDataArray(roles, roleInsertRule, messagesEn);
     if (status) {
         return await insertObjects("roles", roles);
     }
@@ -198,80 +131,15 @@ export async function insertRoles(roles: Array<role>): Promise<{ data: Object | 
 
 
 export async function updateRoles(roles: Array<{ [key: string]: any }>): Promise<{ data: Object | null, status: boolean, errorCode: string | Object }> {
-        const roleRule: RuleSchema = {
-        id: {
-            type: 'string',
-            required: true,
-            format: 'uuid'
-        },
-        name: {
-            type: 'string',
-            required: false,
-            max: 50,
-            noCheckXSS: false
-        },
-        code: {
-            type: 'string',
-            required: false,
-            max: 50
-        },
-        description: {
-            type: 'string',
-            required: false,
-        },
-        createdAt: {
-            type: 'string',
-            required: false,
-            format: 'date'
-        },
-        createdBy: {
-            type: 'string',
-            required: false,
-            max: 100
-        }
-    }
-
-    const { status, results } = validateDataArray(roles, roleRule, messagesEn);
+    const { status, results } = validateDataArray(roles, roleUpdateAndDeleteRule, messagesEn);
     if (status) {
-         return await updateObjects("roles", roles, ["id"]);
+        return await updateObjects("roles", roles, ["id"]);
     }
     return { data: null, status: status, errorCode: { failData: results } };
 }
 
 export async function deleteRoles(roles: Array<{ [key: string]: any }>): Promise<{ data: Object | null, status: boolean, errorCode: string | Object }> {
-        const roleRule: RuleSchema = {
-        id: {
-            type: 'string',
-            required: true,
-            format: 'uuid'
-        },
-        name: {
-            type: 'string',
-            required: false,
-            max: 50,
-            noCheckXSS: false
-        },
-        code: {
-            type: 'string',
-            required: false,
-            max: 50
-        },
-        description: {
-            type: 'string',
-            required: false,
-        },
-        createdAt: {
-            type: 'string',
-            required: false,
-            format: 'date'
-        },
-        createdBy: {
-            type: 'string',
-            required: false,
-            max: 100
-        }
-    }
-    const { status, results } = validateDataArray(roles, roleRule, messagesEn);
+    const { status, results } = validateDataArray(roles, roleUpdateAndDeleteRule, messagesEn);
     if (status) {
 
         return await deleteObjects("roles", roles);
